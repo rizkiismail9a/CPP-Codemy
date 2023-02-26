294b22c9b6628ba95b8349cf65dcd014660f9319
rizkiismail <laksmimentari@gmail.com>
2023-02-26 20:45:54 +0700

    diganti tanda baca

--- Bab2No1.cpp
+++ Bab2No1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     string nama;
-    cout << "Masukkan nama: ";
+    cout << "Masukkan nama! ";
     cin>>nama;
     cout << "Halo "<<nama<<", Selamat datang di ITB!";
     return 0;
